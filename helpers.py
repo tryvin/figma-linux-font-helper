0788c77d2bb333fd29df65833011f960baa91b36
Vin Souza <vin.souza@scalablepath.com>
2019-03-13 20:43:27 -0300

    Initial commit with the HTTP version

---
+++ helpers.py
@@ -0,0 +1,112 @@
+# Figma Font Helper
+# Maintainer Vin <github.com/tryvin>
+# Helper functions
+# BSD license
+
+import re
+import subprocess
+import os
+
+
+def is_valid_origin(origin):
+    if origin is None:
+        return True
+
+    if origin.endswith("/"):
+        origin = origin[:-1]
+
+    return re.match(
+        (
+            '^https?:\\/\\/(?:(?:\\w+\\.)?figma.com|localhost|'
+            '127\\.0\\.0\\.1)(?::\\d+)?$'
+        ),
+        origin
+    )
+
+
+def get_font_extra_information(file_name):
+    font_extra_info = {}
+
+    file_list = " ".join(["\"" + x + "\"" for x in file_name])
+
+    [font_scan_result_status, font_scan_result] = \
+        subprocess.getstatusoutput('fc-scan %s' % (file_list,))
+
+    if font_scan_result_status == 0:
+        font_weight = re.findall("weight: (\\d*)", font_scan_result)
+        font_style = re.findall("style: \"(.*?)\"", font_scan_result)
+
+        element_index = 0
+        for file_element in file_name:
+            if len(file_name) > element_index:
+                font_extra_info[file_element] = {
+                    "weight": font_weight.pop(),
+                    "style": font_style.pop()
+                }
+
+                element_index = element_index + 1
+            else:
+                break
+
+    return font_extra_info
+
+
+def get_font_list():
+    font_list = {}
+
+    for font_line in subprocess.getoutput('fc-list').split("\n"):
+        [font_file, font_info] = font_line.split(":", 1)
+        # Only allows TTF or TTC files, we look at the extension
+        # But we could extend this to look for the mime type
+
+        if font_file.endswith(".ttf") or font_file.endswith(".ttc"):
+            if font_file not in font_list:
+                font_info = font_info.strip()
+                font_extra_info = ""
+
+                if font_info.find(",") > -1:
+                    [font_family, font_extra_info] = font_info.split(",", 1)
+                elif font_info.find(":") > -1:
+                    [font_family, font_extra_info] = font_info.split(":", 1)
+                else:
+                    font_family = font_info
+
+                font_base_name, font_extension = \
+                    os.path.splitext(
+                        os.path.basename(font_file)
+                    )
+
+                font_list[font_file] = [{
+                    "localizedFamily": font_family.replace("\\", ""),
+                    "postscript": font_base_name.replace(" ", ""),
+                    "style": "Regular",
+                    "weight": 200,
+                    "stretch": 5,
+                    "italic": False,
+                    "family": font_family.replace("\\", ""),
+                    "localizedStyle": "Regular"
+                }]
+
+    font_extra_info = get_font_extra_information(list(font_list.keys()))
+    for font_extra_info_name in font_extra_info:
+        font_list[font_extra_info_name][0]['style'] = \
+            font_extra_info[font_extra_info_name]['style']
+
+        font_list[font_extra_info_name][0]['localizedStyle'] = \
+            font_extra_info[font_extra_info_name]['style']
+
+        font_list[font_extra_info_name][0]['italic'] = \
+            True if \
+            font_extra_info[font_extra_info_name]['style'].find("Italic") > -1 \
+            else False
+
+        if len(font_extra_info[font_extra_info_name]['weight']) > 0:
+            font_list[font_extra_info_name][0]['weight'] = \
+                int(font_extra_info[font_extra_info_name]['weight'])
+
+    return font_list
+
+
+if __name__ == "__main__":
+    print("Font List:")
+    print(get_font_list())
---
+++ server.py
@@ -0,0 +1,108 @@
+# Figma Font Helper
+# Maintainer Vin <github.com/tryvin>
+# BSD license
+
+import io
+import os
+from flask import Flask, jsonify, send_file, request, make_response
+
+from helpers import get_font_list, is_valid_origin
+
+
+app = Flask(__name__)
+
+HTTP_PORT = 18412
+HTTPS_PORT = 7335
+PROTOCOL_VERSION = 17
+FONT_FILES = get_font_list()
+
+
+def answers_with_404():
+    return ('', 404)
+
+
+@app.route("/figma/version")
+def version():
+    if is_valid_origin(request.referrer):
+        response = make_response(jsonify({
+            "version": PROTOCOL_VERSION
+        }))
+
+        if request.referrer:
+            response.headers['Access-Control-Allow-Origin'] = \
+                request.referrer[:-1] if request.referrer.endswith("/") else \
+                request.referrer[:-1]
+
+        response.headers['Content-Type'] = 'application/json'
+
+        return response
+    else:
+        return answers_with_404()
+
+
+@app.route("/figma/font-files")
+def font_files():
+    if is_valid_origin(request.referrer):
+        response = make_response(jsonify({
+            "version": PROTOCOL_VERSION,
+            "fontFiles": FONT_FILES
+        }))
+
+        if request.referrer:
+            response.headers['Access-Control-Allow-Origin'] = \
+                request.referrer[:-1] if request.referrer.endswith("/") else \
+                request.referrer[:-1]
+
+        response.headers['Content-Type'] = 'application/json'
+
+        return response
+    else:
+        return answers_with_404()
+
+
+@app.route("/figma/font-file")
+def font_file():
+    file_name = request.args.get("file")
+
+    if file_name:
+        if file_name in FONT_FILES:
+            with open(file_name, 'rb') as bites:
+                response = make_response(send_file(
+                    io.BytesIO(bites.read()),
+                    attachment_filename=os.path.basename(file_name),
+                    mimetype='application/octet-stream'
+                ))
+
+                if request.referrer:
+                    response.headers['Access-Control-Allow-Origin'] = \
+                        request.referrer[:-1] if request.referrer.endswith("/") else \
+                        request.referrer[:-1]
+
+                response.headers['Content-Type'] = 'application/json'
+
+                return response
+
+    return ('', 404)
+
+
+@app.route("/figma/update")
+def need_update():
+    if is_valid_origin(request.referrer):
+        response = make_response(jsonify({
+            "version": PROTOCOL_VERSION
+        }))
+
+        if request.referrer:
+            response.headers['Access-Control-Allow-Origin'] = \
+                request.referrer[:-1] if request.referrer.endswith("/") else \
+                request.referrer[:-1]
+
+        response.headers['Content-Type'] = 'application/json'
+
+        return response
+    else:
+        return answers_with_404()
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=HTTP_PORT)
